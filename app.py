cc32d7e5c47fc835d72acfa4de436ba1beabfad3
COS <csandlin02@gmail.com>
2022-01-30 11:21:32 -0600

    Working App
    works once

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request, flash, redirect
 from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
+import json
+import os
 
 
 app = Flask(__name__)
@@ -8,7 +10,7 @@ Bootstrap(app)
 
 
 ### Database Stuff ###
-app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+app.secret_key = b'noRussian'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 
@@ -33,8 +35,24 @@ def data():
     if request.method == 'GET':
         return f"The URL /data is accessed directly. Try going to '/form' to submit form"
     if request.method == 'POST':
-        form_data = request.form
+        jsonFile = "view.json" ## tests.json
+        form_data = request.form.to_dict(flat=False)
+        try:
+            with open(jsonFile, 'a') as f:
+                json.dump(form_data, f)
+        except:
+            with open(jsonFile, 'w') as d:
+                json.dump(form_data, d)
+
         return render_template('data.html',form_data = form_data)
+
+@app.route('/view')
+def test():
+    title = "View your information" ## Tests
+    with open('view.json') as json_file:
+        dict_data = json.load(json_file)
+    json_file.close()   
+    return render_template('view.html', title=title, data=dict_data)
 
 
 
