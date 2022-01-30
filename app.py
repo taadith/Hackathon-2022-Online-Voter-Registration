6425fca3814e9567a118055e3c64153e5359fda6
COS <csandlin02@gmail.com>
2022-01-30 09:47:23 -0600

    app with home, reg, and show your registration
    need to save registration to file, and be able to lookup in file

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
+from optparse import Values
+from tkinter import INSERT
 from flask import Flask, render_template, request, flash, redirect
 from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
 
-from flask_wtf import FlaskForm
-from wtforms import SubmitField, SelectField, RadioField, HiddenField, StringField, IntegerField, FloatField
-from wtforms.validators import InputRequired, Length, Regexp, NumberRange, DataRequired
-from datetime import date
 
 app = Flask(__name__)
 Bootstrap(app)
@@ -16,36 +14,30 @@ app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 
-
-##
-db = SQLAlchemy(app)
-class reg(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
-    name = db.Column(db.String(100))
-    info = db.Column(db.String(100))
-
-    def __repr__(self):
-        return '<Name %r>' % self.name
-
-class infoForm(FlaskForm):
-    name = StringField("Name", validators=[DataRequired()])
-    Email = StringField("Email", validators=[DataRequired()])
-    submit = SubmitField("Submit")
-#######
+### Form for Registration ###
+form_data = {
+'key1(field1_name)' : 'value1(field1_value)',
+'key2(field2_name)' : 'value2(field2_value)',
+}
+#####
 
 ### Routes ###
-@app.route('/beep', methods=['GET', "POST"])
-def add_info():
-    form = infoForm()
-    return render_template('.html', form = form)
-@app.route('/') ## home page
-def home():
-    return render_template('page_1.html')
+@app.route('/home')
+def index():
+    return render_template('Page_1.html')
 
-@app.route("/register",  methods =["GET", "POST"]) ## voter registration page
+@app.route('/vreg')
 def register():
-    
-    return render_template('registertovote.html');
+    return render_template('registertovote.html')
+
+@app.route('/data', methods = ['POST', 'GET'])
+def data():
+    if request.method == 'GET':
+        return f"The URL /data is accessed directly. Try going to '/form' to submit form"
+    if request.method == 'POST':
+        form_data = request.form
+        return render_template('data.html',form_data = form_data)
+
 
 
 
