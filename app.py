f97e55132dfb433afbf4a3385a35e4f4270eadbf
COS <csandlin02@gmail.com>
2022-01-30 03:45:19 -0600

    Current Main page
    cant figure out the database

--- app.py
+++ app.py
@@ -1,52 +1,59 @@
-from flask import Flask, render_template, url_for, request, redirect, flash
+from optparse import Values
+from tkinter import INSERT
+from flask import Flask, render_template, request, flash, redirect
 from flask_sqlalchemy import SQLAlchemy
+from flask_bootstrap import Bootstrap
+
+from flask_wtf import FlaskForm
+from wtforms import SubmitField, SelectField, RadioField, HiddenField, StringField, IntegerField, FloatField
+from wtforms.validators import InputRequired, Length, Regexp, NumberRange, DataRequired
+from datetime import date
 
 app = Flask(__name__)
+Bootstrap(app)
+
+
 ### Database Stuff ###
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 
-db = SQLAlchemy(app)
 
-class Registry(db.Model):
-    apptype = db.Column(db.String, primary_key=True) ## New, Change in Address/Location in County, Replacement Voter Card
-    interestelectionw = db.Column(db.Integer, unique=True, nullable=False) ## Yes/No
-    firstName = db.Column(db.String(120), unique=True, nullable=False)
-    middleName = db.Column(db.String(120), unique=True, nullable=True) #optional
-    lastName = db.Column(db.String(120), unique=True, nullable=False) # including suffix
-    formerName = db.Column(db.String(120), unique=True, nullable=True) #optional
-    DOB = db.Column(db.String(120), unique=True, nullable=False) ## mm/dd/yyyy
-    gender = db.Column(db.String(120), unique=True, nullable=True) ## male/female
-    phonenumber = db.Column(db.String(10), unique=True, nullable=True) ## areacode:3, middle:3, last: 4 try to split
-    dln = db.Column(db.String(8), unique=True, nullable=False) ## drivers license num
-    ssn4 = db.Column(db.String(4), unique=True, nullable=False) ## or last 4 of ssn
-    resAddress = db.Column(db.String(120), unique=True, nullable=False) ##
-    resCounty = db.Column(db.String(120), unique=True, nullable=False)
-    resZip = db.Column(db.String(9), unique=True, nullable=False) ## first 5, last 4 SPLIT
-    formerCityCounty = db.Column(db.String(120), unique=True, nullable=False) ## Austin, Travis
-    mailingAddress = db.Column(db.String(120), unique=True, nullable=False) ## Address, City
-    mailing = db.Column(db.String(120), unique=True, nullable=True) ## if res is not mailing addy format: Address, City, State, two box zip
+##
+db = SQLAlchemy(app)
+class reg(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(100))
+    info = db.Column(db.String(100))
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<Name %r>' % self.name
 
-
+class infoForm(FlaskForm):
+    name = StringField("Name", validators=[DataRequired()])
+    Email = StringField("Email", validators=[DataRequired()])
+    submit = SubmitField("Submit")
 #######
 
 ### Routes ###
-
-@app.route('/')
+@app.route('/beep', methods=['GET', "POST"])
+def add_info():
+    form = infoForm()
+    return render_template('.html', form = form)
+@app.route('/') ## home page
 def home():
-    return render_template('Page_1.html')
+    return render_template('page_1.html')
 
-@app.route("/register")
+@app.route("/register",  methods =["GET", "POST"]) ## voter registration page
 def register():
-    return render_template('2ndpage.html');
+    
+    return render_template('registertovote.html');
+
 
 
 
 #######
 
 if __name__ == "__main__":
+    db.create_all()
     app.run(debug=True)
